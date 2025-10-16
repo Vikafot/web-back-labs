e4608cdeb14b037f5d3e91b7b388cbe1c0c0a885
Фот Виктория <fot.vicctoria@gmail.com>
2025-10-16 09:10:53 +0700

    Арифметика

--- lab4.py
+++ lab4.py
@@ -34,3 +34,93 @@ def div():
     result = x1 / x2
 
     return render_template('lab4/div.html', x1=x1, x2=x2, result=result)
+
+def safe_int(val, default=None):
+    """Преобразует строку в int или возвращает default, если невозможно."""
+    if val == '' or val is None:
+        return default
+    try:
+        return int(val)
+    except ValueError:
+        return None
+
+
+@lab4.route('/lab4/add')
+def add():
+    a = safe_int(request.args.get('a'), 0)
+    b = safe_int(request.args.get('b'), 0)
+    if a is None or b is None:
+        result = "Ошибка: введите целые числа"
+    else:
+        result = a + b
+    return render_template('lab4/arithmetic.html',
+                           operation="Суммирование",
+                           a=request.args.get('a', ''),
+                           b=request.args.get('b', ''),
+                           result=result,
+                           show_form=True)
+
+
+@lab4.route('/lab4/mul')
+def mul():
+    a = safe_int(request.args.get('a'), 1)
+    b = safe_int(request.args.get('b'), 1)
+    if a is None or b is None:
+        result = "Ошибка: введите целые числа"
+    else:
+        result = a * b
+    return render_template('lab4/arithmetic.html',
+                           operation="Умножение",
+                           a=request.args.get('a', ''),
+                           b=request.args.get('b', ''),
+                           result=result,
+                           show_form=True)
+
+
+@lab4.route('/lab4/sub')
+def sub():
+    a_str = request.args.get('a')
+    b_str = request.args.get('b')
+    if not a_str or not b_str:
+        result = "Ошибка: оба поля должны быть заполнены"
+    else:
+        a = safe_int(a_str)
+        b = safe_int(b_str)
+        if a is None or b is None:
+            result = "Ошибка: введите целые числа"
+        else:
+            result = a - b
+    return render_template('lab4/arithmetic.html',
+                           operation="Вычитание",
+                           a=a_str or '',
+                           b=b_str or '',
+                           result=result,
+                           show_form=True)
+
+
+@lab4.route('/lab4/pow')
+def power():
+    a_str = request.args.get('a')
+    b_str = request.args.get('b')
+    if not a_str or not b_str:
+        result = "Ошибка: оба поля должны быть заполнены"
+    else:
+        a = safe_int(a_str)
+        b = safe_int(b_str)
+        if a is None or b is None:
+            result = "Ошибка: введите целые числа"
+        elif a == 0 and b == 0:
+            result = "Ошибка: 0⁰ не определено"
+        else:
+            try:
+                result = a ** b
+                if abs(result) > 10**10:
+                    result = "Результат слишком большой"
+            except OverflowError:
+                result = "Результат слишком большой"
+    return render_template('lab4/arithmetic.html',
+                           operation="Возведение в степень",
+                           a=a_str or '',
+                           b=b_str or '',
+                           result=result,
+                           show_form=True)
