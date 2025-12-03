7eaeb6b6607b116cd3106f113f4da65b61cb1811
Фот Виктория <fot.vicctoria@gmail.com>
2025-12-03 17:13:38 +0700

    Вывод таблицы

---
+++ static/lab7/main.js
@@ -0,0 +1,41 @@
+function fillFilmList() {
+    fetch('/lab7/rest-api/films/')
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (films) {
+            let tbody = document.getElementById('film-list');
+            tbody.innerHTML = '';
+
+            for (let i = 0; i < films.length; i++) {
+                let tr = document.createElement('tr');
+
+                let tdTitle = document.createElement('td');
+                let tdTitleRus = document.createElement('td');
+                let tdYear = document.createElement('td');
+                let tdActions = document.createElement('td');
+
+                tdTitle.innerText = films[i].title == films[i].title_ru ? '' : films[i].title;
+                tdTitleRus.innerText = films[i].title_ru;
+                tdYear.innerText = films[i].year;
+
+                let editButton = document.createElement('button');
+                editButton.innerText = 'редактировать';
+                editButton.onclick = function () { editFilm(i); };
+
+                let delButton = document.createElement('button');
+                delButton.innerText = 'удалить';
+                delButton.onclick = function () { deleteFilm(i, films[i].title_ru); };
+
+                tdActions.appendChild(editButton);
+                tdActions.appendChild(delButton);
+
+                tr.appendChild(tdTitle);
+                tr.appendChild(tdTitleRus);
+                tr.appendChild(tdYear);
+                tr.appendChild(tdActions);
+
+                tbody.appendChild(tr);
+            }
+        })
+}
