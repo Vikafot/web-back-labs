cde99389ef11daeff81d7b2d572a2f2465ce13d3
Фот Виктория <fot.vicctoria@gmail.com>
2025-12-06 18:06:55 +0700

    Регистрация

--- db/models.py
+++ db/models.py
@@ -3,7 +3,7 @@ from . import db
 class users(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     login = db.Column(db.String(30), nullable=False, unique=True)
-    password = db.Column(db.String(120), nullable=False)
+    password = db.Column(db.String(255), nullable=False)
 
 class articles(db.Model):
     id = db.Column(db.Integer, primary_key=True)
--- lab8.py
+++ lab8.py
@@ -1,4 +1,7 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect
+from db import db
+from db.models import users, articles
+from werkzeug.security import check_password_hash, generate_password_hash
 
 lab8 = Blueprint('lab8', __name__,
                  static_folder='static',
@@ -13,9 +16,32 @@ def lab8_index():
 def login():
     return "Страница входа"
 
-@lab8.route('/register')
+@lab8.route('/register/', methods=['GET', 'POST'])
 def register():
-    return "Страница регистрации"
+    if request.method == 'GET':
+        return render_template('lab8/register.html')
+
+    login_form = request.form.get('login')
+    password_form = request.form.get('password')
+
+    if not login_form or not login_form.strip():
+        return render_template('lab8/register.html', 
+                               error='Имя пользователя не может быть пустым')
+
+    if not password_form or not password_form.strip():
+        return render_template('lab8/register.html', 
+                               error='Пароль не может быть пустым')
+
+    login_exists = users.query.filter_by(login=login_form).first()
+    if login_exists:
+        return render_template('lab8/register.html', 
+                            error='Такой пользователь уже существует')
+
+    password_hash = generate_password_hash(password_form)
+    new_user = users(login=login_form, password=password_hash)
+    db.session.add(new_user)
+    db.session.commit()
+    return redirect('/lab8')
 
 @lab8.route('/articles')
 def articles():
